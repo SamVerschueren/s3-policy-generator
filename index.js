bbb00a1c721b74e3406604a080d9b40e3f17d716
Sam Verschueren <sam.verschueren@gmail.com>
2015-07-24 21:03:31 +0200

    Addes start of the handler

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * This lambda function returns a policy document and signature that can be used to
+ * upload an object directly to S3 from a website. It also returns a unique name that can
+ * be used as key for S3.
+ * 
+ * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @since  27 Jul. 2015
+ */
+
+// module dependencies
+
+/**
+ * Main entrypoint of the service.
+ * 
+ * @param {object}  event       The data regarding the event.
+ * @param {object}  context     The AWS Lambda execution context.
+ */
+exports.handler = function(event, context) {
+    
+};
